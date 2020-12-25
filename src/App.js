a8d7692293d07e92ede214acfa25d18e92636561
PhilCodesNow <philipmayojr@outlook.com>
2020-12-25 14:51:21 -0500

    created basic outlook and input components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Output from "./Output";
+import Input from "./Input";
+
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>Budget app</h1>
+    <div className="app">
+      <Output/>
+      <Input/>
     </div>
   );
 }
---
+++ src/Output.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Output() {
+    return (
+        <div className="output">
+            Output
+        </div>
+    )
+}
+
+export default Output
