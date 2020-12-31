73f2e8522c287d8c66e36d9d54bde1d4d2175246
PhilCodesNow <philipmayojr@outlook.com>
2020-12-31 09:32:29 -0500

    current laptop changes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-
+import react, { useState } from 'react';
 import './App.css';
 import Output from "./Output";
 import Input from "./Input";
@@ -7,8 +7,19 @@ import Header from "./Header";
 
 
 function App() {
+
+
+function checkState() {
+  console.log('check state')
+  budgetItems = budgetItems + 'budget item'
+  console.log(budgetItems)
+}
+
+
+const [budgetItems, setBudgetItems] = useState([]);
   return (
     <div className="app">
+      <button onClick={checkState}>Check State</button>
       <div className="app__header">
         <Header />
       </div>
--- src/Input.js
+++ src/Input.js
@@ -2,6 +2,8 @@ import React from 'react';
 import "./Input.css";
 
 function Input() {
+
+
     return (
         <div className="input">
             <div className="input__header">
