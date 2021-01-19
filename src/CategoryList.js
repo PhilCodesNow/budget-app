59f280682b511b92874f42ab1fc1765399a1f6bc
PhilCodesNow <philipmayojr@outlook.com>
2021-01-19 14:39:13 -0500

    fixed category list map error with key

--- src/CategoryList.js
+++ src/CategoryList.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Category from './Category';
 import "./CategoryList.css";
 import CategoryListAdd from './CategoryListAdd';
+import { v4 as uuidv4 } from 'uuid';
 
 function CategoryList(props) {
     const {
@@ -26,6 +27,7 @@ function CategoryList(props) {
             budgetCategories.map((budgetItem) => {
                 return (
                     <Category 
+                        key={uuidv4()}
                         CategoryName={budgetItem}
                         budgetItemRef={budgetItemRef}
                         handleAddBudgetItem={handleAddBudgetItem}
